c3c9cc62d4124cc93293131a1a8d01bc5c503a45
probablyjassin <aouanijassin@gmail.com>
2024-10-07 16:56:11 +0200

    Refactor hash page layout and styling

--- pages/hash.vue
+++ pages/hash.vue
@@ -1,20 +1,25 @@
 <template>
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="p-8 rounded shadow-md w-full max-w-md">
-            <h1 class="text-2xl font-bold mb-4">Welcome to Student Plan Campus Dual</h1>
-            <p class="text-gray-700">
-                This page provides information about the dual campus plan for students. Here, you will find all the necessary details to navigate through the program and make the most out of your educational experience.
-            </p>
-        </div>
-    </div>
+	<div class="min-h-screen flex items-center justify-center space-x-4">
+		<div class="p-8 rounded shadow-md w-full max-w-md bg-secondary">
+			<h1 class="text-primary text-xl font-bold mb-4">Wie finde ich mein Passwort (Hash)?</h1>
+			<p class="text-text mb-4">
+				Der Hash der für CampusDual wie ein Passwort funktioniert, ist in den URL-Parametern der Anfragen enthalten. 
+                Drücke CTRL+Shift+I um die Konsole zu öffnen und öffne unter "Netzwerk" die Anfrage. <br><br>
+                Diesen Hash benutzt du hier im Login als Passwort.
+			</p>
+		</div>
+		<div class="w-1/2">
+			<img src="/images/hash.webp" class="object-cover rounded w-full" />
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-    name: 'HashPage',
-};
+	export default {
+		name: "HashPage",
+	};
 </script>
 
 <style scoped>
-/* Add any additional styles here */
+	/* Add any additional styles here */
 </style>
