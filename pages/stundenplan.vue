68d773eee257c911513dcc5a2dc8adf670fcb2f7
probablyjassin <aouanijassin@gmail.com>
2024-10-07 14:33:23 +0200

    remove unused comments and logs

--- pages/stundenplan.vue
+++ pages/stundenplan.vue
@@ -22,8 +22,6 @@
 
         data.value.forEach((item) => {
             const day = new Date(item["start"] * 1000).setHours(0, 0, 0, 0);
-            console.log(`start: ${new Date(item["start"] * 1000).toLocaleString()}`);
-            console.log(`end: ${new Date(item["end"] * 1000).toLocaleString()}`);
 
             if (!groupedByDay[day]) {
                 groupedByDay[day] = []
@@ -38,25 +36,9 @@
             })
         })
 
-        console.log(Object.values(groupedByDay))
         return Object.values(groupedByDay)
     })
-
-    /* data = { 
-    "title": "5CS-ZSPLM-11", 
-    "start": 1727762400, 
-    "end": 1727767800, 
-    "allDay": false, 
-    "description": "Zentrales Stundenplanungsmodul", 
-    "color": "orange", 
-    "editable": false, 
-    "room": "205 Seminarraum", 
-    "sroom": "5SR 205", 
-    "instructor": "Prof. Susanne Schneider", 
-    "sinstructor": "Schneider", 
-    "remarks": "Einführung in das Studium durch die Studiengangleitung" 
-    } */
-
+    
     onMounted(async() => {
         const response = await $fetch(`https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Froom%2Fjson%3Fuserid%3D${username.value}%26hash%3D${password.value}`)
         data.value = response
