15f468f76963562c34f9f0582a7bdcf49eec2a89
Jässin <s5002669@ba-sachsen.de>
2024-10-07 11:10:26 +0200

    added stundenplan page, improve login

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,15 @@
 <template>
-    <p>Login</p>
-    <input type="text" name="username" id="username" v-model="username">
-    <input type="password" name="Hash" id="hash" v-model="password">
-    <button @click="login()">Login</button>
+    <p class="text-3xl">Login</p>
+    <p>Benutzername: Matrikelnummer (500xxx)</p>
+    <div>
+        <p>Passwort: Hash von der Campus-Dual API</p> 
+        <NuxtLink to="/hash" class="text-blue-400 underline">(Wie bekomme ich den?)</NuxtLink>
+    </div>
+    <div class="flex flex-col space-y-5">
+        <input type="text" name="username" id="username" placeholder="Username" v-model="username">
+        <input type="password" name="Hash" id="hash" placeholder="Passwort (Hash)" v-model="password">
+        <button @click="login()" class="p-5 bg-pink-600">Login</button>
+    </div>
     <NuxtLink to="/">Home</NuxtLink>
 </template>
 
@@ -10,11 +17,16 @@
     const username = ref("")
     const password = ref("")
 
+    const usernameCookie = useCookie("username")
+    const passwordCookie = useCookie("password")
+
     async function login() {
         const test_url = `https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Fdash%2Fgetcp%3Fuser%3D${username.value}%26hash%3D${password.value}`
         const response = await $fetch(test_url)
         
         if (response == 0) {
+            usernameCookie.value = username.value
+            passwordCookie.value = password.value
             return console.log("yayy")
         }
         return console.log("no")
---
+++ pages/stundenplan.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>stundenplan yayyy:</p>
+        <p>{{data}}</p>
+    </div>
+</template>
+
+<script setup>
+    const data = []
+
+    onMounted(async() => {
+        const response = await $fetch("")
+    })
+</script>
