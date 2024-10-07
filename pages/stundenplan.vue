f8c7f201b1307ba9c49a2af78496a4727ccc2850
Jässin <s5002669@ba-sachsen.de>
2024-10-07 11:22:43 +0200

    fetching studenplan works

--- pages/stundenplan.vue
+++ pages/stundenplan.vue
@@ -6,9 +6,12 @@
 </template>
 
 <script setup>
-    const data = []
+    const username = useCookie("username")
+    const password = useCookie("password")
+    const data = ref([])
 
     onMounted(async() => {
-        const response = await $fetch("")
+        const response = await $fetch(`https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Froom%2Fjson%3Fuserid%3D${username.value}%26hash%3D${password.value}`)
+        data.value = response
     })
 </script>
