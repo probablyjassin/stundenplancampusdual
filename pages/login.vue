146cd9398ac773081ef06cb5ebeda1ce74dd8e3b
Jässin <s5002669@ba-sachsen.de>
2024-10-07 10:16:32 +0200

    basic login page

---
+++ pages/login.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Login</p>
+    <input type="password" name="Hash" id="hash" v-model="password">
+    <button @click="console.log(password)">Login</button>  
+</template>
+
+<script setup>
+    const password = ref("")
+</script>
