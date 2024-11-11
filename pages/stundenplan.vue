d2e27079d02c7efc94163bc89824ef1992c52ffd
Jässin Aouani <aouanijassin@gmail.com>
2024-11-11 12:09:07 +0100

    trying to add datepicker

--- pages/stundenplan.vue
+++ pages/stundenplan.vue
@@ -4,89 +4,107 @@
 			<p class="text-3xl font-bold mb-4">Stundenplan</p>
 			<p class="text-lg mb-4" v-show="data.length">
 				{{
-					new Date(Number(Object.keys(groupedByDay)[page - 1])).toLocaleDateString("de-DE", { weekday: "long", year: "numeric", month: "long", day: "numeric" })
+					new Date(Number(Object.keys(groupedByDay)[page - 1])).toLocaleDateString("de-DE", {
+						weekday: "long",
+						year: "numeric", month: "long", day: "numeric"
+					})
 				}}
 			</p>
 			<span class="mb-4 flex text-center items-center space-x-4">
-				<UPagination v-model="page" :total="data.length" />
-				<UButton
-					v-if="Object.keys(groupedByDay).indexOf(today.toString()) !== -1"
-					:class="{ 'opacity-0': page == Object.keys(groupedByDay).indexOf(today.toString())+1 }"
-					class="py-2"
-					@click="page = Object.keys(groupedByDay).indexOf(today.toString())+1">
+				<div>
+					<UPagination v-model="page" :total="data.length" />
+					<UPopover :popper="{ placement: 'bottom-start' }">
+						<UButton icon="i-heroicons-calendar-days-20-solid" :label="date" />
+
+						<template #panel="{ close }">
+							<DatePicker v-model="pickedDate" is-required @close="close" />
+						</template>
+					</UPopover>
+				</div>
+				{{ pickedDate }}
+
+				<UButton v-if="Object.keys(groupedByDay).indexOf(today.toString()) !== -1"
+					:class="{ 'opacity-0': page == Object.keys(groupedByDay).indexOf(today.toString()) + 1 }"
+					class="py-2" @click="page = Object.keys(groupedByDay).indexOf(today.toString()) + 1">
 					Zurück zu Heute
 				</UButton>
 			</span>
-			<UTable :rows="schedule[page - 1]" class="w-full" :loading="!hasLoaded" :loading-state="{ label: 'Laden...' }" />
+			<UTable :rows="schedule[page - 1]" class="w-full" :loading="!hasLoaded"
+				:loading-state="{ label: 'Laden...' }" />
 			<button @click="logout()" class="mt-5 p-2 bg-primary text-white rounded">Anmeldedaten zurücksetzen</button>
 		</div>
 	</ClientOnly>
 </template>
 
 <script setup>
-	const router = useRouter();
+const router = useRouter();
 
-	const username = useCookie("username");
-	const password = useCookie("password");
+const username = useCookie("username");
+const password = useCookie("password");
 
-	const today = new Date().setHours(0, 0, 0, 0);
-	const page = ref(1);
+const today = new Date().setHours(0, 0, 0, 0);
+const page = ref(1);
+const pickedDate = ref(today);
 
-	const groupedByDay = useState('groupedByDay', (() => ({})));
-	const data = useState('stundenplan', (() => []));
-	const schedule = computed(() => Object.values(groupedByDay.value));
+const groupedByDay = useState('groupedByDay', (() => ({})));
+const data = useState('stundenplan', (() => []));
+const schedule = computed(() => Object.values(groupedByDay.value));
 
-	const hasLoaded = useState('hasLoaded', (() => false));
+const hasLoaded = useState('hasLoaded', (() => false));
 
-	watch(groupedByDay.value, (newVal) => {
-		const todayIndex = Object.keys(newVal).indexOf(today.toString());
-		if (todayIndex !== -1) {
-			page.value = todayIndex + 1;
+watch(groupedByDay.value, (newVal) => {
+	const todayIndex = Object.keys(newVal).indexOf(today.toString());
+	if (todayIndex !== -1) {
+		page.value = todayIndex + 1;
+	} else {
+		const futureDates = Object.keys(newVal).filter(date => Number(date) > today);
+		if (futureDates.length > 0) {
+			page.value = Object.keys(newVal).indexOf(futureDates[0]) + 1;
 		} else {
-			const futureDates = Object.keys(newVal).filter(date => Number(date) > today);
-			if (futureDates.length > 0) {
-				page.value = Object.keys(newVal).indexOf(futureDates[0]) + 1;
-			} else {
-				page.value = 1;
-			}
+			page.value = 1;
 		}
-	});
-
-	onMounted(async () => {
-		if (data.value.length !== 0) return;
-
-		const response = await $fetch(
-			`https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Froom%2Fjson%3Fuserid%3D${username.value}%26hash%3D${password.value}%26t%3D${Math.floor(Date.now() / 1000)}%26_%3D${Date.now()}`
-		);
-		data.value = response;
-
-		data.value.forEach((item) => {
-			const day = new Date(item["start"] * 1000).setHours(0, 0, 0, 0);
-
-			if (!groupedByDay.value[day]) {
-				groupedByDay.value[day] = [];
-			}
-			groupedByDay.value[day].push({
-				Kurs: item["description"],
-				Raum: item["sroom"],
-				Dozent: item["sinstructor"] || "---",
-				Von: new Date(item["start"] * 1000).toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", hour12: false }),
-				Bis: new Date(item["end"] * 1000).toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", hour12: false }),
-				Bemerkungen: item["remarks"] || "---",
-			});
-		});
-		for (const key in groupedByDay.value) {
-			if (Number(key) < new Date().setHours(0, 0, 0, 0) - 86400000) {
-				delete groupedByDay.value[key];
-			}
-		}
-
-		hasLoaded.value = true;
-	});
-
-	function logout() {
-		username.value = "";
-		password.value = "";
-		router.push("/login");
 	}
+});
+
+watch(pickedDate.value, (newVal) => {
+	console.log(newVal);
+});
+
+onMounted(async () => {
+	if (data.value.length !== 0) return;
+
+	const response = await $fetch(
+		`https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Froom%2Fjson%3Fuserid%3D${username.value}%26hash%3D${password.value}%26t%3D${Math.floor(Date.now() / 1000)}%26_%3D${Date.now()}`
+	);
+	data.value = response;
+
+	data.value.forEach((item) => {
+		const day = new Date(item["start"] * 1000).setHours(0, 0, 0, 0);
+
+		if (!groupedByDay.value[day]) {
+			groupedByDay.value[day] = [];
+		}
+		groupedByDay.value[day].push({
+			Kurs: item["description"],
+			Raum: item["sroom"],
+			Dozent: item["sinstructor"] || "---",
+			Von: new Date(item["start"] * 1000).toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", hour12: false }),
+			Bis: new Date(item["end"] * 1000).toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit", hour12: false }),
+			Bemerkungen: item["remarks"] || "---",
+		});
+	});
+	for (const key in groupedByDay.value) {
+		if (Number(key) < new Date().setHours(0, 0, 0, 0) - 86400000) {
+			delete groupedByDay.value[key];
+		}
+	}
+
+	hasLoaded.value = true;
+});
+
+function logout() {
+	username.value = "";
+	password.value = "";
+	router.push("/login");
+}
 </script>
