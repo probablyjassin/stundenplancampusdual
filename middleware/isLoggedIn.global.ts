ee7f3647c298a89a3ea3631c5ed5e590ed13be27
Jässin Aouani <aouanijassin@gmail.com>
2024-11-12 12:59:08 +0100

    trying again

--- middleware/isLoggedIn.global.ts
+++ middleware/isLoggedIn.global.ts
@@ -3,7 +3,23 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 	if (to.fullPath !== "/stundenplan") {
 		return true
 	}
+
+	const username = useCookie("username")
+	const password = useCookie("password")
 	const isLoggedIn = useState("isLoggedIn", (() => false) as () => boolean)
+
+	if (from.fullPath === "/stundenplan") {
+		try {
+			const test_url = `https://corsproxy.io/?https%3A%2F%2Fselfservice.campus-dual.de%2Fdash%2Fgetcp%3Fuser%3D${username.value}%26hash%3D${password.value}`
+			const response = await $fetch(test_url)
+			isLoggedIn.value = !!(response == 0)
+			return true
+		} catch (error) {
+			isLoggedIn.value = false
+			return
+		}
+	}
+
 
 	if (isLoggedIn.value) return true
 	return navigateTo("/login")
