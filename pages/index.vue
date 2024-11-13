99b68529060a3a4063e7cba37e85600c7fa78bcd
probablyjassin <aouanijassin@gmail.com>
2024-11-13 16:42:12 +0100

    changed some css

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,20 @@
 <template>
-    <div class="container mx-auto p-4">
-        <div class="flex flex-col space-y-5">
-            <p class="text-4xl text-primary font-semibold mb-2">Stundenplan für CampusDual</p>
-            <p>
-                Eine simple WebApp, für den Stundenplan von CampusDual. <br>
-                Mit einem Design, dass auf Mobile auch tatsächlich nutzbar ist.
-            </p>
-            <p>
-                Die Seite ist in NuxtJS mit TailwindCSS geschrieben. <br>
-                Anders als auf CampusDual, werden die Daten nur einmal gefetched; <br>
-                zwischen Einträgen wechseln dauert also keine Ewigkeit!
-            </p>
-            <span>
-                <p>Zum Github-Repo:</p>
-                <a class="text-accent underline"
-                    href="https://github.com/probablyjassin/studenplancampusdual">probablyjassin/stundenplancampusdual</a>
-            </span>
-        </div>
-    </div>
+	<div class="container p-4">
+		<div class="flex flex-col space-y-5">
+			<p class="text-4xl text-primary font-semibold mb-2">Stundenplan für CampusDual</p>
+			<p>
+				Eine simple WebApp, für den Stundenplan von CampusDual. <br />
+				Mit einem Design, dass auf Mobile auch tatsächlich nutzbar ist.
+			</p>
+			<p>
+				Die Seite ist in NuxtJS mit TailwindCSS geschrieben. <br />
+				Anders als auf CampusDual, werden die Daten nur einmal gefetched; <br />
+				zwischen Einträgen wechseln dauert also keine Ewigkeit!
+			</p>
+			<span>
+				<p>Zum Github-Repo:</p>
+				<a class="text-accent underline" href="https://github.com/probablyjassin/studenplancampusdual">probablyjassin/stundenplancampusdual</a>
+			</span>
+		</div>
+	</div>
 </template>
