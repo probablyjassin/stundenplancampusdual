f52be5db4f78a89a4e9b421af26cbdba5882de00
Jässin <jaessin.aouani@wbs-it.de>
2025-03-07 09:05:35 +0100

    add note

--- pages/dash/index.vue
+++ pages/dash/index.vue
@@ -5,6 +5,7 @@
             <UIcon name="i-heroicons-arrow-up-left" />
         </span>
         <p>Hier kommen vielleicht noch andere Infos hin, sofern campusdual es supported.</p>
+        <!-- https://selfservice.campus-dual.de/dash/getexamstats?user=5002669&hash=11fd3cf6e9aaa96ee227e969abb03d06 -->
     </div>
 </template>
 
