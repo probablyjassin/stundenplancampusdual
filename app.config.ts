983d36820ae53f5061d51670d3a972385dc7deaf
probablyjassin <aouanijassin@gmail.com>
2025-04-06 00:11:00 +0200

    fix nuxt ui color theme not applying

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,10 @@ export default defineAppConfig({
 	ui: {
 		primary: "pink",
 		gray: "stone",
+		colors: {
+			primary: "pink",
+			secondary: "purple", // optional
+			neutral: "stone", // use this instead of 'gray'
+		},
 	},
 });
