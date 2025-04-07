fa02e585474913d328b379e27c714e4c5c801ee1
Jässin Aouani <aouanijassin@gmail.com>
2025-04-07 11:24:29 +0200

    formatting app.vue

--- app.vue
+++ app.vue
@@ -7,28 +7,28 @@
 </template>
 
 <script setup>
-	useHead({
-		title: "Stundenplan",
-		meta: [
-			{
-				name: "description",
-				content: "Einfache, mobile-first WebApp für den CampusDual-Stundenplan. Support für Firefox und Chromium.",
-			},
-		],
-	});
+useHead({
+	title: "Stundenplan",
+	meta: [
+		{
+			name: "description",
+			content: "Einfache, mobile-first WebApp für den CampusDual-Stundenplan. Support für Firefox und Chromium.",
+		},
+	],
+});
 
-	const { getCampusData } = useCampus();
+const { getCampusData } = useCampus();
 
-	const username = useCookie("username");
-	const password = useCookie("password");
-	const isLoggedIn = useState("isLoggedIn", () => false);
+const username = useCookie("username");
+const password = useCookie("password");
+const isLoggedIn = useState("isLoggedIn", () => false);
 
-	onMounted(async () => {
-		try {
-			const response = await getCampusData("credits");
-			isLoggedIn.value = !!(typeof response === "number");
-		} catch (error) {
-			isLoggedIn.value = false;
-		}
-	});
+onMounted(async () => {
+	try {
+		const response = await getCampusData("credits");
+		isLoggedIn.value = !!(typeof response === "number");
+	} catch (error) {
+		isLoggedIn.value = false;
+	}
+});
 </script>
