9292c1f69a7beaa1b44b4e6ee122cea2441cbb08
Jässin Aouani <aouanijassin@gmail.com>
2025-04-09 11:01:25 +0200

    fix mistake in date format

--- composables/useCalendarDate.ts
+++ composables/useCalendarDate.ts
@@ -2,7 +2,7 @@ import { CalendarDate } from "@internationalized/date";
 
 export function useCalendarDate() {
     function parseCalendarDate(date: CalendarDate): Date {
-        const [year, day, month] = date.toString().split('-')
+        const [year, month, day] = date.toString().split('-')
         return new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
     }
     return {
